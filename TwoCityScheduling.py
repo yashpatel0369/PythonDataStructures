7617be30985115d141735e89fb763f342ebc14f0
Yash Patel <56058553+yashpatel0369@users.noreply.github.com>
2020-06-03 17:05:21 -0400

    Update TwoCityScheduling.py
    There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1]. A function to return the minimum cost to fly every person to a city such that exactly N people arrive in each city.

--- TwoCityScheduling.py
+++ TwoCityScheduling.py
@@ -1,6 +1,6 @@
 # Two City Scheduling
-# Ther are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
-# Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
+# There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
+# A function to return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
 
 class Solution(object):
     def twoCitySchedCost(self, costs):
