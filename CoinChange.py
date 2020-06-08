6fcb5a6782aa9e0065d6b134b83a2cc663e44051
Yash Patel <56058553+yashpatel0369@users.noreply.github.com>
2020-06-08 03:07:51 -0400

    Update CoinChange.py
    Reduced the runtime by 40%

--- CoinChange.py
+++ CoinChange.py
@@ -6,7 +6,6 @@ class Solution:
         dp = [0] * (amount + 1)
         dp[0] = 1
         for i in coins:
-            for j in range(1, amount + 1):
-                if j >= i:
-                    dp[j] += dp[j - i]
+            for j in range(i, amount + 1):
+                dp[j] += dp[j - i]
         return dp[amount]
