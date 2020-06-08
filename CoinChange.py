9fc9f366ed2ad2b408745dace90c495a0df83f23
Yash Patel <56058553+yashpatel0369@users.noreply.github.com>
2020-06-08 02:56:21 -0400

    Create CoinChange.py
    Given coins of different denominations and a total amount of money, a function to compute the number of combinations that make up that amount.

---
+++ CoinChange.py
@@ -0,0 +1,12 @@
+# Given coins of different denominations and a total amount of money, a function to compute the number of combinations that make up that amount.
+# Assume infinite number of each kind of coins.
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in coins:
+            for j in range(1, amount + 1):
+                if j >= i:
+                    dp[j] += dp[j - i]
+        return dp[amount]
